4d678add8e3524e9e93c9d07f9e28747960d8e25
Abdellah Rekoune <arekoune@c2r7p6.1337.ma>
2025-02-01 11:41:04 +0100

    checking the empty lines in the map

--- bonus/checking_map_bonus.c
+++ bonus/checking_map_bonus.c
@@ -6,7 +6,7 @@
 /*   By: arekoune <arekoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 10:29:34 by arekoune          #+#    #+#             */
-/*   Updated: 2025/01/30 13:56:55 by arekoune         ###   ########.fr       */
+/*   Updated: 2025/02/01 11:30:21 by arekoune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,10 +101,10 @@ t_map	*get_map_info(int map_fd, t_map_lst	**map_lst)
 	line = get_next_line(map_fd);
 	while (line)
 	{
-		if (!is_empty(line, false))
+		if (!is_empty(line))
 			flag = set_map_elements(map, line, map_lst);
 		else if (flag == 1)
-			is_last_line(map_fd);
+			exit(ft_write("Error: Empty line in the map content\n", 1));
 		line = get_next_line(map_fd);
 	}
 	return (map);
